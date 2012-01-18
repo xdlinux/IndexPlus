4d512ea196e97e549f8d8415a64f05cda0efee59
bigeagle <justin.w.xd@gmail.com>
2012-01-19 03:47:38 +0800

    时间戳

---
+++ js/jquery.timeago.zh-cn.js
@@ -0,0 +1,19 @@
+// Simplified Chinese
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: "从现在开始",
+  suffixAgo: "前",
+  suffixFromNow: null,
+  seconds: "不到 1 分钟",
+  minute: "1 分钟",
+  minutes: "%d 分钟",
+  hour: "1 小时",
+  hours: "%d 小时",
+  day: "1 天",
+  days: "%d 天",
+  month: "约 1 个月",
+  months: "%d 月",
+  year: "约 1 年",
+  years: "%d 年",
+  numbers: []
+}
